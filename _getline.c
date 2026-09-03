86d44a898f4dc7309521f58c223017f825b76c6b
Malakai-lp <gam3rsuknight@gmail.com>
2026-09-03 14:49:01 +1000

    getline changes removed fgetc to use read made a simple realloc func

--- _getline.c
+++ _getline.c
@@ -1,6 +1,31 @@
 #include "main.h"
 
 /**
+ * remall - expands buffer if over cap
+ * @lineptr: pointer to the input buffer
+ * @n: pointer to the buffer capacity
+ * @new_line: extra buffer to write to
+ * @count: size of lineptr
+ *
+ */
+void remall(size_t *n, char **new_line, size_t *count, char **lineptr)
+{
+	size_t i;
+
+	*n *= 2;
+	new_line = malloc(*n);
+	if (!new_line)
+		return;
+	i = 0;
+	while (i < *count)
+	{
+		*new_line[i] = (*lineptr)[i];
+		i++;
+	}
+	free(*lineptr);
+	*lineptr = *new_line;
+}
+/**
  * _getline - reads one line from a stream
  * @lineptr: pointer to the input buffer
  * @n: pointer to the buffer capacity
@@ -9,14 +34,13 @@
  */
 ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 {
-	int c;
+	char c;
 	char *new_line;
-	size_t i;
+	int res;
 	size_t count = 0;
 
 	if (!lineptr || !n || !stream)
 		return (-1);
-
 	if (!*lineptr)
 	{
 		*n = 128;
@@ -24,26 +48,22 @@ ssize_t _getline(char **lineptr, size_t *n, FILE *stream)
 		if (!*lineptr)
 			return (-1);
 	}
-	while ((c = fgetc(stream)) != EOF)
+	res = read(0, &c, 1);
+	while (res >= 0)
 	{
+		if (res == 0)
+		{
+			printf("\n");
+			break;
+		}
 		if (count + 1 >= *n)
 		{
-			*n *= 2;
-			new_line = malloc(*n);
-			if (!new_line)
-				return (-1);
-			i = 0;
-			while (i < count)
-			{
-				new_line[i] = (*lineptr)[i];
-				i++;
-			}
-			free(*lineptr);
-			*lineptr = new_line;
+			remall(n, &new_line, &count, lineptr);
 		}
-		(*lineptr)[count++] = (char)c;
+		(*lineptr)[count++] = c;
 		if (c == '\n')
 			break;
+		res = read(0, &c, 1);
 	}
 	if (count == 0 && c == EOF)
 		return (-1);
